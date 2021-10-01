b7c9c585e054b4a7fc60f29d603f42b9e9f11b76
Aditya M. Deshpande <10423256+adipandas@users.noreply.github.com>
2021-09-30 20:30:13 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+extras = {
+    'mujoco': [],
+}
+
+# dependency
+all_dependencies = []
+for group_name in extras:
+    all_dependencies += extras[group_name]
+extras['all'] = all_dependencies
+
+setup(name='gym_multirotor',
+      version='0.0.1',
+      url='https://adipandas.github.io',
+      author='Aditya M. Deshpande',
+      author_email='adityadeshpande2010@gmail.com',
+      install_requires=[
+          'matplotlib',
+          'scipy',
+          'imageio',
+          'numpy',
+          'baselines',
+          'tensorboardX',
+          'PyYAML'
+      ],
+      packages=['gym_multirotor', ],
+      extras_require=extras,
+      )
+ 
